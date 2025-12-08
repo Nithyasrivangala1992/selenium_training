cc1d4555775cc6e7798953e04fa4e60fcac93ed8
Nithyasrivangala1992 <vangalanithyasri@gmail.com>
2025-12-08 21:23:44 +0530

    range of the numbers

--- training/learning.py
+++ training/learning.py
@@ -1,6 +1,6 @@
-num = int(input("Enter the number : "))
-if num%2==0:
-    print(f"{num} is even")
+for num in range(100, 200):
+    if num%2==0:
+       print(f"{num} is even")
 else:
-    print(f"{num} is odd")
+      print(f"{num} is odd")
     
