6762f0c37348888e2edb058a1669f644b4d21c5e
Nithyasrivangala1992 <vangalanithyasri@gmail.com>
2025-12-09 10:43:30 +0530

    class Numbers are defined

--- training/learning.py
+++ training/learning.py
@@ -1,7 +1,10 @@
-def even_odd(start, end):
-    for num in range(40, 100):
-        if num%2==0:
-           print(f"{num} is even")
-        else:
-           print(f"{num} is odd")
-even_odd(40, 100)
+class Numbers:
+    def even_odd(self,start, end):
+        for num in range(40, 60):
+            if num%2==0:
+               print(f"{num} is even")
+            else:
+               print(f"{num} is odd")
+
+num = Numbers()
+num.even_odd(40, 60)
